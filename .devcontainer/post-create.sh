cc0b3591a34ca98696a4e020d6de444a7dd3082f
Robert Ayrapetyan <robert@yag.im>
2026-09-05 06:31:06 +0000

    fix: update pydocker

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -4,6 +4,3 @@ mkdir -p /workspaces/jukeboxsvc/.vscode
 cp /workspaces/jukeboxsvc/.devcontainer/vscode/* /workspaces/jukeboxsvc/.vscode
 
 make bootstrap
-
-# TODO: drop after merge: https://github.com/docker/docker-py/pull/3270
-patch /workspaces/jukeboxsvc/.venv/lib/python3.11/site-packages/docker/types/services.py /workspaces/jukeboxsvc/patch_docker_services.diff
